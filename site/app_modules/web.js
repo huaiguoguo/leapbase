21731bb55f1938659c69ff9db0dec7785de04169
leapon <leapon@gmail.com>
2015-08-21 06:45:16 -0400

    add about page, set title in page object

--- site/app_modules/web.js
+++ site/app_modules/web.js
@@ -11,7 +11,14 @@ module.exports = function(app) {
     block.data = tool.object(require('basedata')(app, moduleName));
     block.page = tool.object(require('basepage')(app, moduleName, block.data));
     
+    block.page.getAbout = function(req, res) {
+        var page = { title:'about' };
+        res.render(moduleName + '/about', { page:page });
+    };
+    
     app.server.get('/', block.page.getIndex);
+    app.server.get('/about', block.page.getAbout);
+    
     app.server.get('/' + moduleName + '/page/:pagename', block.page.showPage);
     
     return block;

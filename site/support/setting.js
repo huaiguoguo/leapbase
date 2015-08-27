45097cd1231d981b76e7e0e0d0c7f7d3e9e372f6
leapon <leapon@gmail.com>
2015-08-26 23:04:40 -0400

    add database in comment

--- site/support/setting.js
+++ site/support/setting.js
@@ -5,4 +5,11 @@ exports.setting = {
     http_port: 8080,
     // database
     database: null
+    
+    //database: {
+    //    type: 'mongo',
+    //    host: 'localhost',
+    //    port: 27017,
+    //    name: 'leapbase'
+    //}
 };

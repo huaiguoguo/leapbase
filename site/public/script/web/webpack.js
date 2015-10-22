25b069f6991089e077be5b41facb0876cf89167a
leapon <leapon@gmail.com>
2015-10-22 10:12:45 -0400

    webpack.css is loaded with webpack bundle

--- site/public/script/web/webpack.js
+++ site/public/script/web/webpack.js
@@ -1,5 +1,5 @@
 'use strict'
-
+require('style!css!../../style/web/webpack.css')
 $().ready(function() {
     console.log('in webpack test page');
     var testOutput1 = 'current time: ' + require('./display-time.js');

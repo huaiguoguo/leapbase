293e0864993c23ee019ebea2cb2d89be4be517d4
leapon <leapon@gmail.com>
2015-10-22 10:08:54 -0400

    update on webpack page

--- site/public/script/web/webpack.js
+++ site/public/script/web/webpack.js
@@ -2,5 +2,6 @@
 
 $().ready(function() {
     console.log('in webpack test page');
-    
+    var testOutput1 = 'current time: ' + require('./display-time.js');
+    $('.content1').html(testOutput1);
 });

aa45792bffd2fc41033174fc42d0dfc94498318b
leapon <leapon@gmail.com>
2016-05-13 20:44:25 -0400

    doc folder with sample setting.js added

---
+++ site/doc/setting.js
@@ -0,0 +1,13 @@
+exports.setting = {
+  // web app
+  http_port: 8080,
+  // database
+  database: {
+    type: 'mongo',
+    host: 'localhost',
+    port: 27017,
+    name: 'leapbase',
+    username: '',
+    password: ''
+  }
+};

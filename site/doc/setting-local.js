8281c42821f9b622f1418fca17d877f4cef365e0
leapon <leapon@gmail.com>
2016-05-15 15:12:37 -0400

    setting-local.js added

---
+++ site/doc/setting-local.js
@@ -0,0 +1,14 @@
+exports.setting = {
+  // web app
+  http_port: 8050,
+  invite_code: 'hello',
+  // database
+  database: {
+    type: 'mongo',
+    host: 'localhost',
+    port: 27017,
+    name: 'leapbase',
+    username: '',
+    password: ''
+  }
+};

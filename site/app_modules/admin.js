8112e7272c76da67f1e8c34b65648d7213257922
leapon <leapon@gmail.com>
2016-05-15 14:59:10 -0400

    admin module and index page added; basepage.checkLogin added

--- site/app_modules/admin.js
+++ site/app_modules/admin.js
@@ -14,27 +14,29 @@ module.exports = function(app) {
 
   // site admin page
   block.page.getAdminPage = function(req, res) {
-    var page = app.getPage(req);
+    var page = app.getPage(req, { title:'admin' });
     res.render('admin/index', { page:page });
   };
 
+  /*
   // user manager page
   block.page.getManagePage = function(req, res) {
     var page = app.getPage(req);
     res.render('admin/manage', { page:page });
   };
+  */
 
   // routes
-  /*
   app.server.all('/admin', block.page.checkLogin);
   app.server.all('/admin/*', block.page.checkLogin);
   app.server.get('/admin', block.page.getAdminPage);
-  app.server.get('/' + module_name + '/page/:pagename', block.page.showPage);
 
+  /*
+  app.server.get('/' + module_name + '/page/:pagename', block.page.showPage);
   app.server.all('/manage', block.page.checkLogin);
   app.server.all('/manage/*', block.page.checkLogin);
   app.server.get('/manage', block.page.getManagePage);
   */
-  
+
   return block;
 };

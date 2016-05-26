34cdad4bb3bbc9ce2e246ab5e885e0df67183db7
leapon <leapon@gmail.com>
2016-05-25 20:06:51 -0400

    skip token check on /data/* route if user is logged in already

--- site/app.js
+++ site/app.js
@@ -84,38 +84,42 @@ function setup(cbSetup) {
     cookie: { maxAge: 120 * 60 * 1000 }  //session expires in 120 minutes
   }));
   app.server.use(express.static(path.join(__dirname, app.setting.public_name)));
-  
+
   // DEBUG
   // middleware for api token check
   var apiRoutes = express.Router();
   apiRoutes.use(function(req, res, next) {
-    // check header or url parameters or post parameters for token
-    var token = req.body.token || req.query.token || req.headers['x-access-token'];
-    debug('>>>>> check api token:', token);
-    // decode token
-    if (token) {
-      // verifies secret and checks exp
-      jwt.verify(token, app.setting.token_secret, function(err, decoded) {      
-        if (err) {
-          return res.json({ success: false, message: 'Failed to authenticate token.' });    
-        } else {
-          // if everything is good, save to request for use in other routes
-          req.decoded = decoded;    
-          next();
-        }
-      });
+    if (req.session && req.session.user) {
+      // no need for token check if user is logged in already
+      next();
     } else {
-      // if there is no token, return 403
-      return res.status(403).send({ 
-          success: false, 
-          message: 'No token provided.' 
-      });
+      // check header or url parameters or post parameters for token
+      var token = req.body.token || req.query.token || req.headers['x-access-token'];
+      // decode token
+      if (token) {
+        // verifies secret and checks exp
+        jwt.verify(token, app.setting.token_secret, function(err, decoded) {
+          if (err) {
+            return res.json({ success: false, message: 'Failed to authenticate token.' });
+          } else {
+            console.log('token decoded:', decoded);
+            req.decoded = decoded; // save decoded info in req
+            next();
+          }
+        });
+      } else {
+        // if there is no token, return 403
+        return res.status(403).send({
+            success: false,
+            message: 'No token provided.'
+        });
+      }
     }
   });
-  //app.server.get('/data/*', apiRoutes);
-  app.server.get('/data/api', apiRoutes);
+  app.server.get('/data/*', apiRoutes);
+  //app.server.get('/data/api', apiRoutes);
   // END OF DEBUG
-  
+
   // setup database connection
   if (app.setting.database && app.setting.database.type) {
     var Database = require(app.setting.database.type + '_db');
